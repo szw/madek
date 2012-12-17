1bb6c160ace57835328f353f7b182b7898948159
Ramón Cahenzli <rca@psy-q.ch>
2012-12-17 14:46:41 +0100

    Conflict resolution.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,7 +9,7 @@ set :application, "madek"
 
 set :scm, :git
 set :repository, "git://github.com/zhdk/madek.git"
-set :branch, "next"
+set :branch, "redesign_next"
 set :deploy_via, :remote_cache
 
 set :db_config, "/home/rails/madek-test/database.yml"
